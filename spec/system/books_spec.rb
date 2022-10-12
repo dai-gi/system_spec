cec314cd9e740ff8327e9df2cb48cc4a92057825
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-10-13 06:21:06 +0900

    Modify: ローカル変数の中身をテスト

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Books", type: :system do
   it "enables to visit the index" do
+    book = FactoryBot.create(:book)
     visit books_path
-    expect(page).to have_text("foo")
+    expect(page).to have_text(book.title)
   end
 end

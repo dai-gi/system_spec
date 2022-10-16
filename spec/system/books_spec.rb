5e11d83730a828fe84488e9fcf0025d83d5d553e
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-10-17 00:13:02 +0900

    Modify: 画面を表示せずにシステムテストを実行できるように変更

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Books", type: :system do
+  before do
+    driven_by(:selenium_chrome_headless)
+  end
+
   it "enables to visit the index" do
     book = FactoryBot.create(:book)
     visit books_path
     expect(page).to have_text(book.title)
+  end
+
+  it "enables to create a book" do
+    visit new_book_path
+    fill_in "Title", with: "RubyBook"
+    fill_in "Memo", with: "Nice"
+    click_button "Create Book"
+    expect(page).to have_text("Book successfully created")
   end
 end
